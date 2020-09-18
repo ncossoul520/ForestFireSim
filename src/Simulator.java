ccc17fd0abafa4f6808ed92e066f1f1305e14409
tcossoul <37945015+tcossoul@users.noreply.github.com>
2020-09-18 12:00:54 -0700

    fixed bug in MonteCarloOutput some code cleanup

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,27 +1,18 @@
 import java.util.ArrayList;
 
 public class Simulator {
-
-    /***
-     * Create a new simulator.  The simulator creates a new Forest of size (r, c).
-     *
-     * @param r
-     * @param c
-     */
-
     private static int[][] forest;
     private static int row, col;
 
     public final static int EMPTY_SPACE  = 0; // must be zero for proper initialization
     public final static int LIVING_TREE  = 1;
     public final static int BURNING_TREE = 2;
-    public final static int ASH          = 3;
+    public final static int BURNED_TREE  = 3;
 
-    private int stats_trees = 0;
-    private int stats_alive = 0;
-    private int stats_on_fire = 0;
-    private int stats_burned = 0;
-
+    private int stats_trees   = 0;
+    private int stats_alive   = 0;
+    private int stats_burning = 0;
+    private int stats_burned  = 0;
 
     public Simulator(int r, int c) {
         row = r;
@@ -31,17 +22,19 @@ public class Simulator {
 
 
     public void initialize(int n){
-        stats_trees = (n * row * col) / 100;
+        forest = new int[row][col];  // TODO: ask if better to loop through it to reset to EMPTY_SPACE
+        stats_trees = (row * col * n) / 100;
         stats_alive = stats_trees;
+        stats_burning = 0;
         stats_burned = 0;
-        int num_trees = 0;
 
+        int num_trees = 0;
         do {
-            int tempRow= (int)(Math.random()*row);
-            int tempCol= (int)(Math.random()*col);
-            if ( forest[tempRow][tempCol] == EMPTY_SPACE ){
+            int r = (int)(Math.random()*row);
+            int c = (int)(Math.random()*col);
+            if ( forest[r][c] == EMPTY_SPACE ){
                 num_trees++;
-                forest[tempRow][tempCol] = LIVING_TREE;
+                forest[r][c] = LIVING_TREE;
             }
         } while( num_trees < stats_trees);
     }
@@ -53,12 +46,16 @@ public class Simulator {
             r = (int)(Math.random()*row);
             c = (int)(Math.random()*col);
         } while (forest[r][c] != LIVING_TREE);
+
         forest[r][c] = BURNING_TREE;
+        stats_burning++;
         stats_alive--;
     }
 
     public void setFire(int r, int c) {
         forest[r][c] = BURNING_TREE;
+        stats_burning++;
+        stats_alive--;
     }
 
 
@@ -70,27 +67,25 @@ public class Simulator {
                     // Set on fire each of the neighbors
                     for (int dr = -1; dr <= 1; dr++) {
                         for (int dc = -1; dc <= 1; dc++) {
-                            if ( isInBound(r + dr, c + dc) ) {
-                                if ( forest[r + dr][c + dc] == LIVING_TREE ) {
-                                    Location loc = new Location(r+dr, c+dc);
-                                    if ( !new_fires.contains( loc ) ) {
-                                        new_fires.add(new Location(r + dr, c + dc));
-                                    }
+                            if ( isInBound(r + dr, c + dc) && forest[r + dr][c + dc] == LIVING_TREE ) {
+                                Location loc = new Location(r+dr, c+dc);
+                                if ( !new_fires.contains( loc ) ) {
+                                    new_fires.add( loc );
                                 }
                             }
                         }
                     }
-                    forest[r][c] = ASH;
+                    forest[r][c] = BURNED_TREE;
                     stats_burned++;
                 }
             }
         }
 
-        stats_on_fire = new_fires.size();
-        stats_alive -= stats_on_fire;
         for ( Location loc : new_fires ) {
             forest[ loc.getRow() ][ loc.getCol() ] = BURNING_TREE;
         }
+        stats_burning = new_fires.size();
+        stats_alive -= stats_burning;
     }
 
     private boolean isInBound(int r, int c) {
@@ -98,11 +93,11 @@ public class Simulator {
     }
 
     public void displayStats() {
-        System.out.println( "trees: " + stats_trees + "\talive: " + stats_alive + "\tburned: " + stats_burned + "\ton fire: " + stats_on_fire );
+        System.out.println( "trees: " + stats_trees + "\talive: " + stats_alive + "\tburned: " + stats_burned + "\ton fire: " + stats_burning);
     }
 
     public boolean isOver() {
-        return stats_on_fire == 0;
+        return stats_burning == 0;
     }
 
     public int getWidth() {
@@ -125,12 +120,11 @@ public class Simulator {
         return stats_alive;
     }
 
-    public int getStats_on_fire() {
-        return stats_on_fire;
+    public int getStats_burning() {
+        return stats_burning;
     }
 
     public int getStats_burned() {
         return stats_burned;
     }
-
 }
